326c8a3b8fa5b4f2417612bafc81d80e2cfe9078
flormcalvo <flor.calvo89@gmail.com>
2020-02-07 19:05:58 +1100

    First Commit

---
+++ script.js
@@ -0,0 +1,85 @@
+//We need to cache the DOM: this means storing something for future use.
+//It's good for readability, and mostly so we dont have to run the process to get the reference to that element
+let userScore = 0;
+let computerScore = 0;
+
+const $userScore = document.querySelector("#user-score");
+const $computerScore = document.querySelector("#computer-score");
+const $scoreBoard = document.querySelector(".score-board");
+const $result = document.querySelector(".result > p");
+const $rock = document.querySelector("#r");
+const $paper = document.querySelector("#p");
+const $scissors = document.querySelector("#s");
+
+//adding the event listeners
+function main() {
+  $rock.addEventListener("click", function() {
+    game("r");
+  });
+
+  $paper.addEventListener("click", function() {
+    game("p");
+  });
+
+  $scissors.addEventListener("click", function() {
+    game("s");
+  });
+}
+main();
+
+function game(userChoice) {
+  const computerChoice = getComputerChoice();
+  switch (userChoice + computerChoice) {
+    //first, all situations where the user wins
+    case "rs":
+    case "pr":
+    case "sp":
+      userWins(userChoice, computerChoice);
+      break;
+    case "sr":
+    case "rp":
+    case "ps":
+      userLooses(userChoice, computerChoice);
+      break;
+    case "rr":
+    case "pp":
+    case "ss":
+      draw();
+      break;
+  }
+}
+function getComputerChoice() {
+  const choices = ["r", "p", "s"];
+  const randomNumber = Math.floor(Math.random() * 3);
+  return choices[randomNumber];
+}
+function convertToWord(letter) {
+  if (letter === "r") return "Rock";
+  if (letter === "p") return "Paper";
+  return "Scissors";
+}
+function userWins(userChoice, computerChoice) {
+  userScore++;
+  $userScore.innerHTML = userScore;
+  $computerScore.innerHTML = computerScore;
+  const smallUserWord = "(user)".fontsize(3).sub();
+  const smallCompWord = "(comp)".fontsize(3).sub();
+  //prettier-ignore
+  $result.innerHTML = `${convertToWord(userChoice)}${smallUserWord} beats ${convertToWord(computerChoice)}${smallCompWord}. You win!`;
+  const useR = document.getElementById(userChoice);
+  useR.classList.add("green-glow");
+}
+
+function userLooses(userChoice, computerChoice) {
+  computerScore++;
+  $userScore.innerHTML = userScore;
+  $computerScore.innerText = computerScore;
+  const smallUserWord = "(user)".fontsize(3).sub();
+  const smallCompWord = "(comp)".fontsize(3).sub();
+  //prettier-ignore
+  $result.innerHTML = `${convertToWord(computerChoice)}${smallCompWord} beats ${convertToWord(userChoice)}${smallUserWord}. Computer wins!`;
+}
+
+function draw() {
+  $result.innerHTML = "It's a Draw!";
+}
