c3cbddfb6381f68e2e2dc5db2c1c7d632f9b9539
flormcalvo <flor.calvo89@gmail.com>
2020-02-20 11:14:44 +1100

    changed var names

--- script.js
+++ script.js
@@ -1,30 +1,25 @@
-//We need to cache the DOM: this means storing something for future use.
-//It's good for readability, and mostly so we dont have to run the process to get the reference to that element
 let userScore = 0;
 let computerScore = 0;
 
-const $userScore = document.querySelector("#user-score");
-const $computerScore = document.querySelector("#computer-score");
-const $scoreBoard = document.querySelector(".score-board");
-const $result = document.querySelector(".result > p");
-const $rock = document.querySelector("#r");
-const $paper = document.querySelector("#p");
-const $scissors = document.querySelector("#s");
+const userScoreElement = document.querySelector("#user-score");
+const computerScoreElement = document.querySelector("#computer-score");
+const result = document.querySelector(".result > p");
+const rock = document.querySelector("#r");
+const paper = document.querySelector("#p");
+const scissors = document.querySelector("#s");
 
-//adding the event listeners
 function main() {
-  $rock.addEventListener("click", () => game("r"));
+  rock.addEventListener("click", () => game("r"));
 
-  $paper.addEventListener("click", () => game("p"));
+  paper.addEventListener("click", () => game("p"));
 
-  $scissors.addEventListener("click", () => game("s"));
+  scissors.addEventListener("click", () => game("s"));
 }
 main();
 
 function game(userChoice) {
   const computerChoice = getComputerChoice();
   switch (userChoice + computerChoice) {
-    //first, all situations where the user wins
     case "rs":
     case "pr":
     case "sp":
@@ -54,12 +49,12 @@ function convertToWord(letter) {
 }
 function userWins(userChoice, computerChoice) {
   userScore++;
-  $userScore.innerHTML = userScore;
-  $computerScore.innerHTML = computerScore;
+  userScoreElement.innerHTML = userScore;
+  computerScoreElement.innerHTML = computerScore;
   const smallUserWord = "(user)".fontsize(3).sub();
   const smallCompWord = "(comp)".fontsize(3).sub();
   //prettier-ignore
-  $result.innerHTML = `${convertToWord(userChoice)}${smallUserWord} beats ${convertToWord(computerChoice)}${smallCompWord}. You win!`;
+  result.innerHTML = `${convertToWord(userChoice)}${smallUserWord} beats ${convertToWord(computerChoice)}${smallCompWord}. You win!`;
   const $userBorder = document.getElementById(userChoice);
   $userBorder.classList.add("green-glow");
   setTimeout(() => $userBorder.classList.remove("green-glow"), 1000);
@@ -67,17 +62,17 @@ function userWins(userChoice, computerChoice) {
 
 function userLooses(userChoice, computerChoice) {
   computerScore++;
-  $userScore.innerHTML = userScore;
-  $computerScore.innerText = computerScore;
+  userScoreElement.innerHTML = userScore;
+  computerScoreElement.innerText = computerScore;
   const smallUserWord = "(user)".fontsize(3).sub();
   const smallCompWord = "(comp)".fontsize(3).sub();
   //prettier-ignore
-  $result.innerHTML = `${convertToWord(computerChoice)}${smallCompWord} beats ${convertToWord(userChoice)}${smallUserWord}. Computer wins!`;
+  result.innerHTML = `${convertToWord(computerChoice)}${smallCompWord} beats ${convertToWord(userChoice)}${smallUserWord}. Computer wins!`;
   const $userBorder = document.getElementById(userChoice);
   $userBorder.classList.add("red-glow");
   setTimeout(() => $userBorder.classList.remove("red-glow"), 1000);
 }
 
 function draw() {
-  $result.innerHTML = "It's a Draw!";
+  result.innerHTML = "It's a Draw!";
 }
